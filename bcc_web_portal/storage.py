ccbbea884f0aab3f5d5f408f5dad94670421998e
JakirNobi <jakirnoby@gmail.com>
2026-06-18 18:54:48 +0600

    fix:image quality

--- bcc_web_portal/storage.py
+++ bcc_web_portal/storage.py
@@ -1,26 +1,10 @@
-import os
-
-import cloudinary.uploader
 from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
 class HighQualityCloudinaryStorage(MediaCloudinaryStorage):
-    def _upload(self, name, content):
-        options = {
-            'use_filename': True,
-            'resource_type': self._get_resource_type(name),
-            'tags': self.TAG,
-            'quality': 100,
-            'colorspace': 'srgb',
-        }
-        folder = os.path.dirname(name)
-        if folder:
-            options['folder'] = folder
-        return cloudinary.uploader.upload(content, **options)
-
     def url(self, name):
         url = super().url(name)
         parts = url.split('/upload/')
         if len(parts) == 2:
-            return f"{parts[0]}/upload/q_100/{parts[1]}"
+            return f"{parts[0]}/upload/q_50/{parts[1]}"
         return url
