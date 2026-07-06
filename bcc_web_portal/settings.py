1a0c9bb1266a7a182d963524058579c3de1e7d53
JakirNobi <jakirnoby@gmail.com>
2026-07-06 23:27:22 +0600

    cookie

--- bcc_web_portal/settings.py
+++ bcc_web_portal/settings.py
@@ -73,7 +73,7 @@ LOGIN_URL = '/login/'
 
 # Session: auto-logout on browser close or 20 min inactivity
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_COOKIE_AGE = 1200
+SESSION_COOKIE_AGE = 1800
 SESSION_SAVE_EVERY_REQUEST = True
 
 ROOT_URLCONF = 'bcc_web_portal.urls'
